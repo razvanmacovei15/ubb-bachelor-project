aa872d367624b3c0c859efe2ec8617bdff74993d
Razvan Macovei <razvanmc15@Razvans-MacBook-Pro.local>
2025-03-08 13:00:25 +0200

    first commit

---
+++ follow-the-beat/src/main/java/com/maco/followthebeat/FollowTheBeatApplication.java
@@ -0,0 +1,13 @@
+package com.maco.followthebeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowTheBeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowTheBeatApplication.class, args);
+    }
+
+}
---
+++ follow-the-beat/src/test/java/com/maco/followthebeat/FollowTheBeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maco.followthebeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowTheBeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
